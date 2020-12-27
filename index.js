e69923287ec0e3b8d911b300628b5671a8246057
Hector Pohle <hectorpohle@gmail.com>
2020-12-27 15:23:36 -0300

    agregamos nodemon y rutas

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const app = express();
 
+app.get('/api/athletes', (req, res) => {
+    res.send('Obteniendo el recurso de atletas');
+});
+app.post('/api/athletes', (req, res) => {
+    res.send('Creando un nuevo recurso de atletas');
+});
+
+app.get('/api/sports', (req, res) => {
+    res.send('Obteniendo el recurso de deportes');
+});
+app.post('/api/sports', (req, res) => {
+    res.send('Creando un nuevo recurso de deportes');
+});
+
+
 app.listen(8080, () => {
     console.log('App listening on port 8080');
 });
